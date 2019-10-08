dbbeea430b281d573c2645ec98e23703e53b710e
GitHub Jeff <jefferson2004@gmail.com>
2019-10-08 15:41:56 -0400

    from work

--- 2_titanic.py
+++ 2_titanic.py
@@ -74,12 +74,13 @@ for train_index, test_index in split.split(titanic_df, titanic_df.Sex):
     strat_train_set = titanic_df.loc[train_index]
     strat_test_set = titanic_df.loc[test_index]
 
-strat_test_set.Sex.value_counts()/len(strat_test_set)
-titanic_df.Sex.value_counts()/len(titanic_df)
+strat_test_set.Sex.value_counts() / len(strat_test_set)
+titanic_df.Sex.value_counts() / len(titanic_df)
 
 
 def income_cat_proportions(data):
     return data["Sex"].value_counts() / len(data)
+
 
 train_set, test_set = train_test_split(titanic_df, test_size=0.2, random_state=42)
 
@@ -91,19 +92,11 @@ compare_props = pd.DataFrame({
 compare_props["Rand. %error"] = 100 * compare_props["Random"] / compare_props["Overall"] - 100
 compare_props["Strat. %error"] = 100 * compare_props["Stratified"] / compare_props["Overall"] - 100
 
-
-
 titanic_df = strat_train_set.copy()
 
-# titanic_df.plot(kind="scatter", x="Sex_female", y="Fare", alpha=0.4,
-#     s=titanic_df["Age"].sum()/100, label="Age", figsize=(10,7),
-#     c="Survived", cmap=plt.get_cmap("jet"), colorbar=True,
-#     sharex=False)
-# plt.legend()
-
-# from pandas.plotting import scatter_matrix
-# attributes = ['Survived','Pclass', 'Sex_female', 'Age', 'SibSp', 'Fare', 'Embarked_C']
-# scatter_matrix(titanic_df[attributes], figsize=(12, 8), )
+titanic_df['sibsp_parch'] = (titanic_df.SibSp + titanic_df.Parch)
+titanic_df['fare_per_sibspparch'] = (titanic_df.Fare / (titanic_df.sibsp_parch + 1))
+print(titanic_df)
 
 titanic_df = strat_train_set.drop('Survived', axis=1)
 titanic_df_labels = strat_train_set['Survived'].copy()
@@ -112,15 +105,56 @@ sample_incomplete_rows = titanic_df[titanic_df.isnull().any(axis=1)].head()
 sample_incomplete_rows
 
 from sklearn.impute import SimpleImputer
+
 imputer = SimpleImputer(strategy='median')
+titanic_df_num = titanic_df.select_dtypes(include=[np.number])
+imputer.fit(titanic_df_num)
+print(imputer.statistics_)
+print(titanic_df_num.median().values)
+
+X = imputer.transform(titanic_df_num)
+titanic_df_tr = pd.DataFrame(X, columns=titanic_df_num.columns, index=titanic_df.index)
+titanic_df_tr.loc[sample_incomplete_rows.index.values]
+imputer.strategy
+titanic_df_tr = pd.DataFrame(X, columns=titanic_df_num.columns, index=titanic_df_num.index)
+
+titanic_df_cat = titanic_df[['Sex', 'Embarked']]
+titanic_df_cat.fillna(method='ffill', inplace=True)
+
+from sklearn.preprocessing import OneHotEncoder
+
+cat_encoder = OneHotEncoder(sparse=False)
+titanic_df_1hot = cat_encoder.fit_transform(titanic_df_cat)
+titanic_df_1hot
+
+from sklearn.base import BaseEstimator, TransformerMixin
+
+# column index
+sibsp_ix, parch_ix, fare_ix = 5, 6, 8
 
 
+class CombinedAttributesAdder(BaseEstimator, TransformerMixin):
+    def __init__(self, add_fare_per_sibspparch=True):  # no *args or **kargs
+        self.add_fare_per_sibspparch = add_fare_per_sibspparch
+
+    def fit(self, X, y=None):
+        return self  # nothing else to do
+
+    def transform(self, X, y=None):
+        sibsp_parch = X[:, sibsp_ix] + X[:, parch_ix]
+        if self.add_fare_per_sibspparch:
+            fare_per_sibspparch = X[:, fare_ix] / (X[:, sibsp_ix] + X[:, parch_ix] + 1)
+            return np.c_[X, sibsp_parch, fare_per_sibspparch]
+        else:
+            return np.c_[X, sibsp_parch]
 
 
+attr_adder = CombinedAttributesAdder(add_fare_per_sibspparch=False)
+titanic_df_extra_attribs = attr_adder.transform(titanic_df.values)
 
+titanic_df_extra_attribs = pd.DataFrame(
+    titanic_df_extra_attribs,
+    columns=list(titanic_df.columns) + ['sibsp_parch'],
+    index=titanic_df.index)
 
-
-
-
-
-
+titanic_df_extra_attribs.head()
