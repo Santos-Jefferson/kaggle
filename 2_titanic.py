bc79c9947d66f1eb8b388a89ad5da0b60541bdf9
GitHub Jeff <jefferson2004@gmail.com>
2019-10-09 09:45:08 -0400

    from work

--- 2_titanic.py
+++ 2_titanic.py
@@ -1,136 +1,53 @@
-# Python ≥3.5 is required
+# IMPORTS #########################################################
 import sys
-
-assert sys.version_info >= (3, 5)
-
-# Scikit-Learn ≥0.20 is required
-import sklearn
-
-assert sklearn.__version__ >= "0.20"
-# Common imports
 import numpy as np
 import os
-
-# to make this notebook's output stable across runs
-np.random.seed(42)
 
 # To plot pretty figures
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-
-mpl.rc('axes', labelsize=14)
-mpl.rc('xtick', labelsize=12)
-mpl.rc('ytick', labelsize=12)
-
-# Where to save the figures
-PROJECT_ROOT_DIR = "."
-CHAPTER_ID = "end_to_end_project"
-IMAGES_PATH = os.path.join(PROJECT_ROOT_DIR, "images", CHAPTER_ID)
-os.makedirs(IMAGES_PATH, exist_ok=True)
-
-
-def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
-    path = os.path.join(IMAGES_PATH, fig_id + "." + fig_extension)
-    print("Saving figure", fig_id)
-    if tight_layout:
-        plt.tight_layout()
-    plt.savefig(path, format=fig_extension, dpi=resolution)
-
 
 # Ignore useless warnings (see SciPy issue #5998)
 import warnings
 
 warnings.filterwarnings(action="ignore", message="^internal gelsd")
 
-import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
-import matplotlib.pyplot as plt
+import pandas as pd
 import os
-
-
-def generate_submission_file(testdata, predictions, filename):
-    # predictions = np.where(predictions > 0.5, 1, 0)
-    output = pd.DataFrame({'PassengerId': testdata.PassengerId, 'Survived': predictions})
-    output.to_csv(filename, index=False)
-    print("Your submission was successfully saved!")
-
-
-for dirname, _, filenames in os.walk('input/titanic/'):
-    for filename in filenames:
-        print(os.path.join(dirname, filename))
-
-test_data = pd.read_csv('input/titanic/test.csv')
-titanic_df = pd.read_csv('input/titanic/train.csv')
-
-print(test_data)
-print(titanic_df)
-
-columns_to_use = ['Survived', 'Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'Embarked']
-# titanic_df = pd.get_dummies(titanic_df[columns_to_use])
 
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import StratifiedShuffleSplit
-
-split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
-for train_index, test_index in split.split(titanic_df, titanic_df.Sex):
-    strat_train_set = titanic_df.loc[train_index]
-    strat_test_set = titanic_df.loc[test_index]
-
-strat_test_set.Sex.value_counts() / len(strat_test_set)
-titanic_df.Sex.value_counts() / len(titanic_df)
-
-
-def income_cat_proportions(data):
-    return data["Sex"].value_counts() / len(data)
-
-
-train_set, test_set = train_test_split(titanic_df, test_size=0.2, random_state=42)
-
-compare_props = pd.DataFrame({
-    "Overall": income_cat_proportions(titanic_df),
-    "Stratified": income_cat_proportions(strat_test_set),
-    "Random": income_cat_proportions(test_set),
-}).sort_index()
-compare_props["Rand. %error"] = 100 * compare_props["Random"] / compare_props["Overall"] - 100
-compare_props["Strat. %error"] = 100 * compare_props["Stratified"] / compare_props["Overall"] - 100
-
-titanic_df = strat_train_set.copy()
-
-titanic_df['sibsp_parch'] = (titanic_df.SibSp + titanic_df.Parch)
-titanic_df['fare_per_sibspparch'] = (titanic_df.Fare / (titanic_df.sibsp_parch + 1))
-print(titanic_df)
-
-titanic_df = strat_train_set.drop('Survived', axis=1)
-titanic_df_labels = strat_train_set['Survived'].copy()
-
-sample_incomplete_rows = titanic_df[titanic_df.isnull().any(axis=1)].head()
-sample_incomplete_rows
-
-from sklearn.impute import SimpleImputer
-
-imputer = SimpleImputer(strategy='median')
-titanic_df_num = titanic_df.select_dtypes(include=[np.number])
-imputer.fit(titanic_df_num)
-print(imputer.statistics_)
-print(titanic_df_num.median().values)
-
-X = imputer.transform(titanic_df_num)
-titanic_df_tr = pd.DataFrame(X, columns=titanic_df_num.columns, index=titanic_df.index)
-titanic_df_tr.loc[sample_incomplete_rows.index.values]
-imputer.strategy
-titanic_df_tr = pd.DataFrame(X, columns=titanic_df_num.columns, index=titanic_df_num.index)
-
-titanic_df_cat = titanic_df[['Sex', 'Embarked']]
-titanic_df_cat.fillna(method='ffill', inplace=True)
-titanic_df[['Sex', 'Embarked']] = titanic_df[['Sex', 'Embarked']].fillna(method='ffill')
-
-from sklearn.preprocessing import OneHotEncoder
-
-cat_encoder = OneHotEncoder(sparse=False)
-titanic_df_1hot = cat_encoder.fit_transform(titanic_df_cat)
-titanic_df_1hot
-
 from sklearn.base import BaseEstimator, TransformerMixin
 
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.datasets import make_moons, make_circles, make_classification
+from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+
+mpl.rc('axes', labelsize=14)
+mpl.rc('xtick', labelsize=12)
+mpl.rc('ytick', labelsize=12)
+
+# CONSTANTS ######################################################
+# Where to save the figures
+PROJECT_ROOT_DIR = "."
+CHAPTER_ID = "end_to_end_project"
+IMAGES_PATH = os.path.join(PROJECT_ROOT_DIR, "images", CHAPTER_ID)
+os.makedirs(IMAGES_PATH, exist_ok=True)
+
+# CLASSES #########################################################
+# COMBINE ATTRIBUTES #
 # column index
 sibsp_ix, parch_ix, fare_ix = 3, 4, 5
 
@@ -151,6 +68,94 @@ class CombinedAttributesAdder(BaseEstimator, TransformerMixin):
             return np.c_[X, sibsp_parch]
 
 
+# FUNCTIONS #######################################################
+def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
+    path = os.path.join(IMAGES_PATH, fig_id + "." + fig_extension)
+    print("Saving figure", fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format=fig_extension, dpi=resolution)
+
+
+def generate_submission_file(testdata, predictions, filename):
+    # predictions = np.where(predictions > 0.5, 1, 0)
+    output = pd.DataFrame({'PassengerId': testdata.PassengerId, 'Survived': predictions})
+    output.to_csv(filename, index=False)
+    print("Your submission was successfully saved!")
+
+
+for dirname, _, filenames in os.walk('input/titanic/'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+
+test_data = pd.read_csv('input/titanic/test.csv')
+titanic_df = pd.read_csv('input/titanic/train.csv')
+
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+for train_index, test_index in split.split(titanic_df, titanic_df.Sex):
+    strat_train_set = titanic_df.loc[train_index]
+    strat_test_set = titanic_df.loc[test_index]
+
+# strat_test_set.Sex.value_counts() / len(strat_test_set)
+# titanic_df.Sex.value_counts() / len(titanic_df)
+
+
+# def income_cat_proportions(data):
+#     return data["Sex"].value_counts() / len(data)
+
+# train_set, test_set = train_test_split(titanic_df, test_size=0.2, random_state=42)
+
+# compare_props = pd.DataFrame({
+#     "Overall": income_cat_proportions(titanic_df),
+#     "Stratified": income_cat_proportions(strat_test_set),
+#     "Random": income_cat_proportions(test_set),
+# }).sort_index()
+# compare_props["Rand. %error"] = 100 * compare_props["Random"] / compare_props["Overall"] - 100
+# compare_props["Strat. %error"] = 100 * compare_props["Stratified"] / compare_props["Overall"] - 100
+
+titanic_df = strat_train_set.copy()
+
+titanic_df['sibsp_parch'] = (titanic_df.SibSp + titanic_df.Parch)
+titanic_df['fare_per_sibspparch'] = (titanic_df.Fare / (titanic_df.sibsp_parch + 1))
+# print(titanic_df)
+
+titanic_df = strat_train_set.drop('Survived', axis=1)
+titanic_df_labels = strat_train_set['Survived'].copy()
+
+sample_incomplete_rows = titanic_df[titanic_df.isnull().any(axis=1)].head()
+print('sample incomplete rows')
+print(sample_incomplete_rows)
+
+# SIMPLEIMPUTER ########################################################
+from sklearn.impute import SimpleImputer
+
+imputer = SimpleImputer(strategy='median')
+titanic_df_num = titanic_df.select_dtypes(include=[np.number])  # Get number columns from dataset
+imputer.fit(titanic_df_num)
+print(imputer.statistics_)
+print(titanic_df_num.median().values)
+
+X = imputer.transform(titanic_df_num)
+titanic_df_tr = pd.DataFrame(X, columns=titanic_df_num.columns, index=titanic_df.index)
+# titanic_df_tr.loc[sample_incomplete_rows.index.values]
+print('imputer strategy')
+print(imputer.strategy)
+titanic_df_tr = pd.DataFrame(X, columns=titanic_df_num.columns, index=titanic_df_num.index)
+
+categories = ['Sex', 'Embarked']
+
+titanic_df_cat = titanic_df[categories]
+titanic_df_cat.fillna(method='ffill', inplace=True)
+titanic_df[categories] = titanic_df[categories].fillna(method='ffill')
+
+# ONEHOTENCODER ##########################################################
+from sklearn.preprocessing import OneHotEncoder
+
+cat_encoder = OneHotEncoder(sparse=False)
+titanic_df_1hot = cat_encoder.fit_transform(titanic_df_cat)
+print('one hot encoder with category columns')
+print(titanic_df_1hot)
+
 # attr_adder = CombinedAttributesAdder(add_fare_per_sibspparch=False)
 # titanic_df_extra_attribs = attr_adder.transform(titanic_df_num.values)
 #
@@ -161,21 +166,24 @@ class CombinedAttributesAdder(BaseEstimator, TransformerMixin):
 #
 # titanic_df_extra_attribs.head()
 
+# PIPELINE ############################################################
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 
 num_pipeline = Pipeline([
     ('imputer', SimpleImputer(strategy="median")),
-    ('attribs_adder', CombinedAttributesAdder()),
+    ('attribs_adder', CombinedAttributesAdder(add_fare_per_sibspparch=True)),
     ('std_scaler', StandardScaler()),
 ])
 
 titanic_df_num_tr = num_pipeline.fit_transform(titanic_df_num)
 
+# COLUMNTRANSFORMER (NUMBERS / CATEGORIES) ##############################
+
 from sklearn.compose import ColumnTransformer
 
 num_attribs = list(titanic_df_num)
-cat_attribs = ['Sex', 'Embarked']
+cat_attribs = categories
 
 full_pipeline = ColumnTransformer([
     ("num", num_pipeline, num_attribs),
@@ -183,15 +191,80 @@ full_pipeline = ColumnTransformer([
 ])
 
 titanic_df_prepared = full_pipeline.fit_transform(titanic_df)
-titanic_df_prepared
+print('df prepared')
+print(titanic_df_prepared)
 
-from sklearn.linear_model import LinearRegression, LogisticRegression
-from sklearn.naive_bayes import GaussianNB
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
-from sklearn.svm import SVC
+# ML ALGOS ##########################################################
 
-lin_reg = SVC(degree=15, kernel='rbf', gamma=0.1 )
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import cross_val_score
+
+names = ["Nearest Neighbors", "Linear SVM", "RBF SVM", "Gaussian Process",
+         "Decision Tree", "Random Forest", "Neural Net", "AdaBoost",
+         "Naive Bayes", "QDA"]
+
+classifiers = [
+    KNeighborsClassifier(3),
+    SVC(kernel="linear", C=0.025),
+    SVC(gamma=0.1, C=1),
+    GaussianProcessClassifier(1.0 * RBF(1.0)),
+    DecisionTreeClassifier(max_depth=5),
+    RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
+    MLPClassifier(alpha=1, max_iter=1000),
+    AdaBoostClassifier(),
+    GaussianNB(),
+    QuadraticDiscriminantAnalysis()
+]
+
+for clf, name in zip(classifiers, names):
+    clf.fit(titanic_df_prepared, titanic_df_labels)
+
+    titanic_predictions = clf.predict(titanic_df_prepared)
+    mse = mean_squared_error(titanic_df_labels, titanic_predictions)
+    rmse = np.sqrt(mse)
+    print()
+    print()
+    print('---_' + name + '_RMSE_------------------------------------')
+    print(rmse)
+    print('***********************************************************')
+
+    mae = mean_absolute_error(titanic_df_labels, titanic_predictions)
+    print('---_' + name + '_MAE_------------------------------------')
+    print(mae)
+    print('***********************************************************')
+
+    # # let's try the full preprocessing pipeline on a few training instances
+    # some_data = titanic_df[:5]
+    # some_labels = titanic_df_labels[:5]
+    # some_data_prepared = full_pipeline.transform(some_data)
+    # preds = clf.predict(some_data_prepared)
+    #
+    # print("Predictions_" + name + '_:', preds)
+    # print("Labels_" + name + '_:', list(some_labels))
+
+    clf_scores = cross_val_score(clf, titanic_df_prepared, titanic_df_labels,
+                             scoring="neg_mean_squared_error", cv=10)
+    clf_rmse_scores = np.sqrt(-clf_scores)
+
+    def display_scores(scores):
+        print('------_' + name + '_-------')
+        print("Scores:", scores)
+        print("Mean:", scores.mean())
+        print("Standard deviation:", scores.std())
+        print('=============================================================')
+        print('=============================================================')
+        print('=============================================================')
+        print()
+        print()
+
+    display_scores(clf_rmse_scores)
+
+sys.exit()
+
+
+
+# lin_reg = SVC(degree=15, kernel='rbf', gamma=0.1)
 
 # kneigh = KNeighborsClassifier()
 # randomForest = RandomForestClassifier(n_estimators=200, max_depth=10, random_state=1)
@@ -199,7 +272,6 @@ lin_reg = SVC(degree=15, kernel='rbf', gamma=0.1 )
 # rfreg.fit(titanic_df_prepared, titanic_df_labels)
 # svc_class = SVC(kernel='linear')
 # svc_class.fit(titanic_df_prepared, titanic_df_labels)
-#
 #
 #
 # randomForest.fit(titanic_df_prepared, titanic_df_labels)
