e994cc8cf7dfc0d31bd15ed9c244545bb7bbe76b
jefferson <jefferson2004@gmail.com>
2019-10-09 00:43:22 -0400

    from home desk

--- 2_titanic.py
+++ 2_titanic.py
@@ -48,6 +48,7 @@ import os
 
 
 def generate_submission_file(testdata, predictions, filename):
+    # predictions = np.where(predictions > 0.5, 1, 0)
     output = pd.DataFrame({'PassengerId': testdata.PassengerId, 'Survived': predictions})
     output.to_csv(filename, index=False)
     print("Your submission was successfully saved!")
@@ -184,92 +185,233 @@ full_pipeline = ColumnTransformer([
 titanic_df_prepared = full_pipeline.fit_transform(titanic_df)
 titanic_df_prepared
 
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+from sklearn.svm import SVC
 
-lin_reg = LinearRegression()
-lin_reg = KNeighborsClassifier()
-lin_reg = RandomForestClassifier(n_estimators=200, max_depth=10, random_state=1)
-lin_reg.fit(titanic_df_prepared, titanic_df_labels)
+lin_reg = SVC(degree=15, kernel='rbf', gamma=0.1 )
 
-# let's try the full preprocessing pipeline on a few training instances
-some_data = titanic_df
-some_labels = titanic_df_labels
-some_data_prepared = full_pipeline.transform(some_data)
-preds = lin_reg.predict(some_data_prepared)
-
-print("Predictions:", preds)
-print("Labels:", list(some_labels))
-
-from sklearn.metrics import mean_squared_error
-
-housing_predictions = lin_reg.predict(titanic_df_prepared)
-lin_mse = mean_squared_error(titanic_df_labels, housing_predictions)
-lin_rmse = np.sqrt(lin_mse)
-lin_rmse
-
-from sklearn.metrics import mean_absolute_error
-
-lin_mae = mean_absolute_error(titanic_df_labels, housing_predictions)
-lin_mae
-
-from sklearn.tree import DecisionTreeRegressor
-
-tree_reg = DecisionTreeRegressor(random_state=42)
-tree_reg.fit(titanic_df_prepared, titanic_df_labels)
-
-titanic_predictions = tree_reg.predict(titanic_df_prepared)
-tree_mse = mean_squared_error(titanic_df_labels, titanic_predictions)
-tree_rmse = np.sqrt(tree_mse)
-tree_rmse
-
-
+# kneigh = KNeighborsClassifier()
+# randomForest = RandomForestClassifier(n_estimators=200, max_depth=10, random_state=1)
+# rfreg = RandomForestRegressor(n_estimators=200, random_state=42)
+# rfreg.fit(titanic_df_prepared, titanic_df_labels)
+# svc_class = SVC(kernel='linear')
+# svc_class.fit(titanic_df_prepared, titanic_df_labels)
+#
+#
+#
+# randomForest.fit(titanic_df_prepared, titanic_df_labels)
+#
+# # let's try the full preprocessing pipeline on a few training instances
+# some_data = titanic_df
+# some_labels = titanic_df_labels
+# some_data_prepared = full_pipeline.transform(some_data)
+# preds = randomForest.predict(some_data_prepared)
+#
+# print("Predictions:", preds)
+# print("Labels:", list(some_labels))
+#
+# from sklearn.metrics import mean_squared_error
+#
+# titanic_preds = svc_class.predict(titanic_df_prepared)
+# svm_mse = mean_squared_error(titanic_df_labels, titanic_preds)
+# svm_rmse = np.sqrt(svm_mse)
+# svm_rmse
+#
+# titanic_preds = rfreg.predict(titanic_df_prepared)
+# forest_mse = mean_squared_error(titanic_df_labels, titanic_preds)
+# forest_rmse = np.sqrt(forest_mse)
+# forest_rmse
+#
+# housing_predictions = randomForest.predict(titanic_df_prepared)
+# lin_mse = mean_squared_error(titanic_df_labels, housing_predictions)
+# lin_rmse = np.sqrt(lin_mse)
+# lin_rmse
+#
+# from sklearn.metrics import mean_absolute_error
+#
+# lin_mae = mean_absolute_error(titanic_df_labels, housing_predictions)
+# lin_mae
+#
+# from sklearn.tree import DecisionTreeRegressor
+#
+# tree_reg = DecisionTreeRegressor(random_state=42)
+# tree_reg.fit(titanic_df_prepared, titanic_df_labels)
+#
+# titanic_predictions = tree_reg.predict(titanic_df_prepared)
+# tree_mse = mean_squared_error(titanic_df_labels, titanic_predictions)
+# tree_rmse = np.sqrt(tree_mse)
+# tree_rmse
+#
 from sklearn.model_selection import cross_val_score
 
-scores = cross_val_score(tree_reg, titanic_df_prepared, titanic_df_labels,
-                         scoring="neg_mean_squared_error", cv=10)
-tree_rmse_scores = np.sqrt(-scores)
+#
+# tree_scores = cross_val_score(tree_reg, titanic_df_prepared, titanic_df_labels,
+#                          scoring="neg_mean_squared_error", cv=10)
+# treereg_rmse_scores = np.sqrt(-tree_scores)
+#
+# def display_scores(scores):
+#     print('------treereg----------')
+#     print("Scores:", scores)
+#     print("Mean:", scores.mean())
+#     print("Standard deviation:", scores.std())
+#     print('----------------')
+#
+# display_scores(treereg_rmse_scores)
+#
+# ran_scores = cross_val_score(randomForest, titanic_df_prepared, titanic_df_labels,
+#                          scoring="neg_mean_squared_error", cv=10)
+# randomForest_rmse_scores = np.sqrt(-ran_scores)
+#
+# def display_scores(scores):
+#     print('------randomForest----------')
+#     print("Scores:", scores)
+#     print("Mean:", scores.mean())
+#     print("Standard deviation:", scores.std())
+#     print('----------------')
+#
+# display_scores(randomForest_rmse_scores)
+#
+linreg_scores = cross_val_score(lin_reg, titanic_df_prepared, titanic_df_labels,
+                                scoring="neg_mean_squared_error", cv=10)
+lin_reg_rmse_scores = np.sqrt(-linreg_scores)
+
+
 def display_scores(scores):
-    print()
+    print('------lin_reg----------')
     print("Scores:", scores)
     print("Mean:", scores.mean())
     print("Standard deviation:", scores.std())
-    print()
-
-display_scores(tree_rmse_scores)
-
-lin_scores = cross_val_score(lin_reg, titanic_df_prepared, titanic_df_labels,
-                             scoring="neg_mean_squared_error", cv=10)
-lin_rmse_scores = np.sqrt(-lin_scores)
-display_scores(lin_rmse_scores)
-
-from sklearn.ensemble import RandomForestRegressor
-
-forest_reg = RandomForestRegressor(n_estimators=100, random_state=42)
-forest_reg.fit(titanic_df_prepared, titanic_df_labels)
-
-titanic_predictions = forest_reg.predict(titanic_df_prepared)
-forest_mse = mean_squared_error(titanic_df_labels, titanic_predictions)
-forest_rmse = np.sqrt(forest_mse)
-print(forest_rmse)
-
-from sklearn.model_selection import cross_val_score
-
-forest_scores = cross_val_score(forest_reg, titanic_df_prepared, titanic_df_labels,
-                                scoring="neg_mean_squared_error", cv=10)
-forest_rmse_scores = np.sqrt(-forest_scores)
-display_scores(forest_rmse_scores)
-
-scores = cross_val_score(lin_reg, titanic_df_prepared, titanic_df_labels, scoring="neg_mean_squared_error", cv=10)
-pd.Series(np.sqrt(-scores)).describe()
+    print('----------------')
 
 
-from sklearn.svm import SVR
+display_scores(lin_reg_rmse_scores)
+# sys.exit()
+#
+# rfreg_scores = cross_val_score(rfreg, titanic_df_prepared, titanic_df_labels,
+#                          scoring="neg_mean_squared_error", cv=10)
+# rfreg_rmse_scores = np.sqrt(-rfreg_scores)
+#
+# def display_scores(scores):
+#     print('------rfreg----------')
+#     print("Scores:", scores)
+#     print("Mean:", scores.mean())
+#     print("Standard deviation:", scores.std())
+#     print('----------------')
+#
+# display_scores(rfreg_rmse_scores)
+#
+# svc_scores = cross_val_score(svc_class, titanic_df_prepared, titanic_df_labels,
+#                          scoring="neg_mean_squared_error", cv=10)
+# svc_rmse_scores = np.sqrt(-svc_scores)
+#
+# def display_scores(scores):
+#     print('------svc_class----------')
+#     print("Scores:", scores)
+#     print("Mean:", scores.mean())
+#     print("Standard deviation:", scores.std())
+#     print('----------------')
+#
+# display_scores(svc_rmse_scores)
 
-svm_reg = SVR(kernel="linear")
-svm_reg.fit(titanic_df_prepared, titanic_df_labels)
-titanic_predictions = svm_reg.predict(titanic_df_prepared)
-svm_mse = mean_squared_error(titanic_df_labels, titanic_predictions)
-svm_rmse = np.sqrt(svm_mse)
-print(svm_rmse)
+
+# from sklearn.model_selection import GridSearchCV
+#
+# param_grid = [
+#     # try 12 (3×4) combinations of hyperparameters
+#     {'n_estimators': [100, 200, 300, 400, 1000], 'max_features': [2, 4, 6, 8, 10]},
+#     # then try 6 (2×3) combinations with bootstrap set as False
+#     {'bootstrap': [False], 'n_estimators': [3, 10], 'max_features': [2, 3, 4]},
+# ]
+#
+# forest_reg = RandomForestClassifier(random_state=42)
+# # train across 5 folds, that's a total of (12+6)*5=90 rounds of training
+# grid_search = GridSearchCV(forest_reg, param_grid, cv=5,
+#                            scoring='neg_mean_squared_error',
+#                            return_train_score=True)
+# grid_search.fit(titanic_df_prepared, titanic_df_labels)
+#
+# print('best parameter found')
+# print(grid_search.best_params_)
+# print(grid_search.best_estimator_)
+#
+# cvres = grid_search.cv_results_
+# for mean_score, params in zip(cvres["mean_test_score"], cvres["params"]):
+#     print(np.sqrt(-mean_score), params)
+#
+# pd.DataFrame(grid_search.cv_results_)
+
+# from sklearn.model_selection import RandomizedSearchCV
+# from scipy.stats import randint
+#
+# param_distribs = {
+#     # 'C': randint(0.001, 5.0),
+#     'kernel': ['linear', 'poly', 'rbf', 'sigmoid'],
+#     'degree': randint(low=1, high=100),
+#     # 'gamma': random.randrange(0.01, 1.0),
+#     # 'shrinking ': [True, False],
+#     # 'max_features': randint(low=1, high=8),
+# }
+#
+# forest_reg = SVC()
+# rnd_search = RandomizedSearchCV(forest_reg, param_distributions=param_distribs,
+#                                 n_iter=1, cv=5, scoring='neg_mean_squared_error', random_state=42)
+# rnd_search.fit(titanic_df_prepared, titanic_df_labels)
+#
+# print('best parameter found')
+# print(rnd_search.best_params_)
+# print(rnd_search.best_estimator_)
+# # sys.exit()
+#
+# cvres = rnd_search.cv_results_
+# for mean_score, params in zip(cvres["mean_test_score"], cvres["params"]):
+#     print(np.sqrt(-mean_score), params)
+#
+# pd.DataFrame(rnd_search.cv_results_)
+#
+# feature_importances = rnd_search.best_estimator_.feature_importances_
+# feature_importances
+#
+# extra_attribs = []
+# # cat_encoder = cat_pipeline.named_steps["cat_encoder"] # old solution
+# cat_encoder = full_pipeline.named_transformers_["cat"]
+# cat_one_hot_attribs = list(cat_encoder.categories_[0])
+# attributes = num_attribs + extra_attribs + cat_one_hot_attribs
+# print(sorted(zip(feature_importances, attributes), reverse=True))
+
+# final_model = grid_search.best_estimator_
+# final_model = rnd_search.best_estimator_
+X_test = strat_test_set.drop("Survived", axis=1)
+X_test = test_data
+# y_test = strat_test_set["Survived"].copy()
+
+X_test_prepared = full_pipeline.transform(X_test)
+lin_reg.fit(titanic_df_prepared, titanic_df_labels)
+final_predictions = lin_reg.predict(X_test_prepared)
+generate_submission_file(X_test, final_predictions, 'svc15_0.10_1.csv')
+
+# final_mse = mean_squared_error(y_test, final_predictions)
+# final_rmse = np.sqrt(final_mse)
+
+
+# from scipy import stats
+#
+# confidence = 0.95
+# squared_errors = (final_predictions - y_test) ** 2
+# np.sqrt(stats.t.interval(confidence, len(squared_errors) - 1,
+#                          loc=squared_errors.mean(),
+#                          scale=stats.sem(squared_errors)))
+#
+# m = len(squared_errors)
+# mean = squared_errors.mean()
+# tscore = stats.t.ppf((1 + confidence) / 2, df=m - 1)
+# tmargin = tscore * squared_errors.std(ddof=1) / np.sqrt(m)
+# print(np.sqrt(mean - tmargin), np.sqrt(mean + tmargin))
+#
+# zscore = stats.norm.ppf((1 + confidence) / 2)
+# zmargin = zscore * squared_errors.std(ddof=1) / np.sqrt(m)
+# print(np.sqrt(mean - zmargin), np.sqrt(mean + zmargin))
+#
+#
